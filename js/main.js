c5695ea1be36620807f444757d6b89c909638bfe
Your Name <you@example.com>
2020-01-29 12:28:52 -0500

    par2-1

--- js/main.js
+++ js/main.js
@@ -64,6 +64,42 @@ allInputField.forEach( input => {
 // })
 
 //    4- Advanced: Then do this by writing only ONE event listener/handler for all input elements and applying it using a traversal loop */
-// >>>>>>Added to part 3 advance<<<<<<
+// >>>>>>Added to step 3-advance<<<<<<
 
 // Done Part 1
+
+
+
+
+/*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+
+// ## Part 2
+
+// 1. Modify the `#submit` element from `type="button"` to `type="submit"`. When the user triggers a `submit`, event, _prevent_ the form from proceeding with it's _default_ behavior.
+
+submitBtn.setAttribute(`type`,`submit`);
+submitBtn.addEventListener ("click", (event) => 
+    {
+        event.preventDefault()
+    });
+// 2. On submit, only add the `.error` class to the elements that have a `value` of "" (blank, meaning any text will prevent an error)
+//    - What happens if the user just adding spaces to the field? Find a function that will trim off the white spaces in a field to validate it.
+
+
+
+
+// 3. Prototype a validation by checking the two fields on `submit`. If both are "valid" (not blank), close the `#modal`, remove the `#signin` and append `textContent` to the `#hello` heading so it reads "Welcome, [USERNAME]", in a font that's half the current size
+//    - Consider the various ways we could validate both fields, including the use of a boolean `var` as a switch
+
+
+
+// 4. Try validating a field by adding/removing `.error` immediately after the cursor leaves one of the individual `<input>` (rather than on submit)
+//    - Consider this can be added individually to elements, but do so by writing one event listener/callback function using a loop
+
+
+
+
+// 5. Allow the user to click the `#modal` to close itself
+//    - Consider what happens when two elements that are overlapping are both waiting for a click: Click events will "bubble" (propagate) up the DOM tree. How could we stop an event from triggering on it's parents?
+//    - This will likely require a bit of web searching to solve
